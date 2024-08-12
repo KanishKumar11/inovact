f8a6d1b66d9b05c76f6dbc8c0a28c700e003ad45
Kanish Kumar <kanishkumar11@outlook.in>
2024-08-12 22:10:40 +0530

    minor update

--- src/components/sections/Get.tsx
+++ src/components/sections/Get.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Get = () => {
   return (
     <div className="max-w-7xl mx-auto flex flex-col lg:gap-0 items-center p-5 gap-5">
-      <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-[#1649FF] to-[#0D2C99] lg:text-[150px] text-6xl text-balance">
+      <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-[#1649FF] to-[#0D2C99] lg:text-[150px] text-6xl text-balance text-center">
         Get Inovact Social Now
       </h2>
       <p className="text-xl">
